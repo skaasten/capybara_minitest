667cfe8c11f556c98ef1f25dd32d6b2e580033a8
Shaun Kaasten <shaunk@gmail.com>
2014-05-21 06:18:24 -0400

    Add test for creating a new client.

--- test/login_test.rb
+++ test/login_test.rb
@@ -1,7 +1,9 @@
 require 'minitest/autorun'
 require 'capybara'
 require 'capybara/dsl'
-require 'debugger'
+require 'byebug'
+require 'faker'
+
 
 Capybara.run_server = false
 Capybara.current_driver = :selenium
@@ -9,11 +11,21 @@ Capybara.app_host = 'http://shaunfirst.freshbooks.com'
 
 class LoginTest < Minitest::Test
   include Capybara::DSL
+
   def test_login
     visit '/'
     fill_in 'username', with: ENV['username']
     fill_in 'password', with: ENV['password']
     click_on "Log in"
     assert page.has_content? 'Overview'
+  end
+
+  def test_new_client
+    visit '/createUser'
+    fill_in 'email', with: Faker::Internet.email
+    fill_in 'fname', with: Faker::Name.first_name
+    fill_in 'lname', with: Faker::Name.last_name
+    click_on "Save"
+    assert page.has_content? 'Your client has been created.'
   end
 end
